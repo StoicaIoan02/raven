9cd5a176ca52ea16f46bdfcd4d571bbe3ca2eab9
Ioan <ioan.stoica@kodingtech.com>
2022-07-14 17:05:28 +0300

    select id, limit 1 in loc de count()

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -21,6 +22,17 @@ func checkError(err error) {
 	}
 }
 
+type StartingJob struct {
+	id                       int
+	declaration_id           *int
+	date                     time.Time
+	status                   string
+	tip                      string
+	job_id                   *int
+	reporting_declaration_id *int
+	account_id               int
+}
+
 func checkJobs(tipCautat string) {
 
 	// Initialize connection string.
@@ -35,26 +47,15 @@ func checkJobs(tipCautat string) {
 	checkError(err)
 	fmt.Println("Successfully created connection to database")
 
-	// Numarare declaratii in progres.
-	sql_statement := fmt.Sprintf("SELECT count(id)  from declaration_queue  where status = 'on progress' and type = '%s';", tipCautat)
+	// Selectam prima declaratie in progres.
+	sql_statement := fmt.Sprintf("SELECT id from declaration_queue  where status = 'on progress' and type = '%s' limit 1;", tipCautat)
 	rows, err := db.Query(sql_statement)
 	checkError(err)
 	defer rows.Close()
 
-	var runningJobs int
-	rows.Next()
-
-	switch err := rows.Scan(&runningJobs); err {
-	case sql.ErrNoRows:
-		fmt.Println("No rows were returned")
-	case nil:
-		fmt.Println("Numar declaratii in runningJobs:", runningJobs)
-	default:
-		checkError(err)
-	}
-
-	if runningJobs == 0 {
-		fmt.Println("Nu avem declaratii in progres.")
+	// Daca nu avem nici-o declaratie in progres:
+	if !rows.Next() {
+		fmt.Println("Nu avem declaratii in progres")
 
 		// Declaratii in asteptare.
 		sql_statement := fmt.Sprintf("SELECT count(id)  from declaration_queue  where status = 'queued' and type = '%s';", tipCautat)
@@ -81,35 +82,29 @@ func checkJobs(tipCautat string) {
 			checkError(err)
 			defer rows.Close()
 
-			// startingJob
-			var (
-				id                       sql.NullString
-				declaration_id           sql.NullString
-				date                     sql.NullString
-				status                   sql.NullString
-				tip                      sql.NullString
-				job_id                   sql.NullString
-				reporting_declaration_id sql.NullString
-				account_id               sql.NullString
-			)
-
+			var declaratie StartingJob
 			// Prima declaratie din coada
 			fmt.Println("Prima declaratie: ")
 			rows.Next()
-			switch err := rows.Scan(&id, &declaration_id, &date, &status, &tip, &job_id, &reporting_declaration_id, &account_id); err {
+			switch err := rows.Scan(&declaratie.id, &declaratie.declaration_id, &declaratie.date, &declaratie.status, &declaratie.tip, &declaratie.job_id, &declaratie.reporting_declaration_id, &declaratie.account_id); err {
 			case sql.ErrNoRows:
 				fmt.Println("No rows were returned")
 			case nil:
-				fmt.Println("Data row = (", id.String, ", ", status.String, ")")
+				fmt.Println("Data row = (", declaratie.id, ", ", declaratie.status, ")")
 			default:
 				checkError(err)
 			}
 
+			rows, err = db.Query("count(1);")
+
+			fmt.Println(*declaratie.reporting_declaration_id)
+
 			var url_pad sql.NullString
-			//lrn := ""
+			//var lrn sql.NullString
 
 			if tipCautat == "report" {
-				sql_statement := fmt.Sprintf("select pad_url from users where id = (SELECT users_id _id from reporting_declaration where id = %s );", reporting_declaration_id.String)
+				///Setare pad_url
+				sql_statement := fmt.Sprintf("select pad_url from users where id = (SELECT users_id _id from reporting_declaration where id = %d );", *declaratie.reporting_declaration_id)
 				rows, err := db.Query(sql_statement)
 				checkError(err)
 				defer rows.Close()
@@ -118,10 +113,15 @@ func checkJobs(tipCautat string) {
 				err = rows.Scan(&url_pad)
 				checkError(err)
 
+				if url_pad.Valid == false {
+					fmt.Println("error: url_pad.Valid == false ")
+				}
 				fmt.Println("Pad_url:", url_pad.String)
 
 			}
 		}
+	} else {
+		fmt.Println("Exista deja o declaratie in progres")
 	}
 }
 
